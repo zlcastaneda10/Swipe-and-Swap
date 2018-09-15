c39527d4b95b84f9e6cdd04762242d04663935c5
Juan Camilo Usche <jc.useche10@uniandes.edu.co>
2018-09-15 11:30:16 -0500

    linter conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,33 @@
 module.exports = {
-   "env": {
-       "browser": true
-   },
-   "extends": [
-       "eslint:recommended",
-       "plugin:react/recommended"],
-   "parserOptions": {
-       "ecmaVersion": 6,
-       "sourceType": "module",
-       "ecmaFeatures": {
-           "jsx": true
-       }
-   },
-   "rules": {
-       "indent": [
-           "error",
-           2
-       ],
-       "linebreak-style": [
-           "error",
-           "unix"
-       ],
-       "quotes": [
-           "error",
-           "single"
-       ],
-       "semi": [
-           "error",
-           "always"
-       ],
-       "no-console": 0
-   }
+    "env": {
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            0
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
+    }
 };
