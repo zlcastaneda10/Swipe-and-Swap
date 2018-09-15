c8718a2e48856b8a16ceb7a99c186cc6150a8897
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-09-15 14:22:08 -0500

    Creacion de componente addObject
    para empezar a hacer el formulario

---
+++ front/src/addObject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class addObject extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- front/src/index.js
+++ front/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import SignUp from './SignupForm.js';
 import registerServiceWorker from './registerServiceWorker';
+import addObject from "./addObject.js";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<SignUp/>, document.getElementById('signup'));
+ReactDOM.render(<addObject/>, document.getElementById('addObject'));
 registerServiceWorker();
