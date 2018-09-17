8e886d96a67652dd1399ffc70b419505f9e38442
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-09-17 08:19:47 -0500

    static files react

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'front/build')));
 app.use("/", signup);
 
 app.use('/', indexRouter);
